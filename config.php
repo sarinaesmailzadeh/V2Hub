c58eb886c7c4dd457b283c59cf64e4989933177c
sarina esmailzadeh <141073575+sarinaesmailzadeh@users.noreply.github.com>
2023-08-05 22:33:46 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
 $subscription_urls = [
+  "http://104.168.107.230/aggregate.txt",
   "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
   "https://branch.blanku.me/",
   "https://raw.githubusercontent.com/irancpi/Subscription/main/Sub",
