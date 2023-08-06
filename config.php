59f0cdaeb45befe9ed205d11bdc13d06a5bfe840
sarinaesmailzadeh <sayhernameagain@gmail.com>
2023-08-06 15:36:53 +0330

    update time

--- config.php
+++ config.php
@@ -6,5 +6,4 @@ $subscription_urls = [
   "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
   "https://branch.blanku.me/",
   "https://raw.githubusercontent.com/irancpi/Subscription/main/Sub",
-  "https://raw.githubusercontent.com/amirmohammad-mohammad-88/Sub-Reality-Azadi-config/Config/Azadi-Reality-Different",
 ];
